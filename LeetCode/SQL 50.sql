eedcee096875b89f0a4ac9d2e8b7b0d0a3998025
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2025-11-21 13:18:26 +0200

    2356. Number of Unique Subjects Taught by Each Teacher

--- LeetCode/SQL 50.sql
+++ LeetCode/SQL 50.sql
@@ -8,3 +8,12 @@ LEFT JOIN Confirmations AS c
 ON s.user_id = c.user_id
 GROUP BY s.user_id
 */
+
+-- 2356. Number of Unique Subjects Taught by Each Teacher
+/*
+SELECT
+	teacher_id,
+	COUNT(DISTINCT subject_id) AS cnt 
+FROM Teacher
+GROUP BY teacher_id
+*/
