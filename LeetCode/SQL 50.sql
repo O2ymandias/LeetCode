cf8acd994b6566e9e72bfdadd435d3edef7f8843
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2025-11-21 13:57:05 +0200

    1141. User Activity for the Past 30 Days I

--- LeetCode/SQL 50.sql
+++ LeetCode/SQL 50.sql
@@ -17,3 +17,14 @@ SELECT
 FROM Teacher
 GROUP BY teacher_id
 */
+
+-- 1141. User Activity for the Past 30 Days I
+/*
+SELECT
+	activity_date AS day,
+	COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date <= '2019-07-27' 
+AND DATEDIFF(DAY, activity_date, '2019-07-27') < 30
+GROUP BY activity_date
+*/
