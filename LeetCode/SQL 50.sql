b33af428311e46a65f6c441ba73d9957980042a2
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2025-11-22 22:31:15 +0200

    1251. Average Selling Price

--- LeetCode/SQL 50.sql
+++ LeetCode/SQL 50.sql
@@ -28,3 +28,16 @@ WHERE activity_date <= '2019-07-27'
 AND DATEDIFF(DAY, activity_date, '2019-07-27') < 30
 GROUP BY activity_date
 */
+
+-- 1251. Average Selling Price
+/*
+SELECT 
+	p.product_id,
+	ISNULL(ROUND(SUM(p.price * u.units) * 1.0 / SUM(u.units) * 1.0, 2), 0) AS  average_price
+FROM Prices AS p
+LEFT JOIN UnitsSold AS u
+ON p.product_id = u.product_id
+AND u.purchase_date >= p.start_date
+AND u.purchase_date <= p.end_date
+GROUP BY p.product_id
+*/
