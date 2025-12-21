482174d28aaba44ca7c7446c9da779466b282efd
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2025-12-21 15:06:33 +0200

    1667. Fix Names in a Table

--- LeetCode/SQL 50.sql
+++ LeetCode/SQL 50.sql
@@ -464,3 +464,15 @@ AND NOT EXISTS (
 	AND i.lon = ck.lon
 )
 */
+
+-- 1667. Fix Names in a Table
+/*
+SELECT
+	user_id,
+	CONCAT(
+		SUBSTRING(UPPER(name), 1, 1),
+		SUBSTRING(LOWER(name), 2, LEN(name))
+	) AS name
+FROM Users
+ORDER BY user_id
+*/
