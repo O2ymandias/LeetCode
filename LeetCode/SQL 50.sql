60f37536c3debf87a61e0eaaf16a983e155d2043
Nael Muhamed <ozymandias.ll.kmt@gmail.com>
2026-01-10 14:55:20 +0200

    1581. Customer Who Visited but Did Not Make Any Transactions

--- LeetCode/SQL 50.sql
+++ LeetCode/SQL 50.sql
@@ -58,6 +58,18 @@ INNER JOIN Sales AS s
 ON p.product_id = s.product_id
 */
 
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+/*
+SELECT 
+	v.customer_id,
+	COUNT(v.customer_id) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t
+ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
+*/
+
 -- 1934. Confirmation Rate
 /*
 SELECT
